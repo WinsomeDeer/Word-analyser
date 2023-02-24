0b45b405a2337b3c476b887685bc59798531abc4
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-02-24 19:02:44 +0000

    Update word.cpp

--- word.cpp
+++ word.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <thread>
+using namespace std;
 
+int unique_words(string word){
+    
+}
+
+int main(){
+    string bible = "KJVbible.txt", quran = "quran.txt";
+    ifstream input;
+
+    if(!input.is_open()){
+        return -1;
+    }
+
+    while(input){
+        string line;
+        getline(input, line, ':');
+    }
+
+
+    return 0;
+}
