f2aba5def3a0df7c4b4d0bea2568436a09b59fd6
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-03-06 21:36:49 +0000

    Update word.cpp

--- word.cpp
+++ word.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
 #include <thread>
+#include <sstream>
+#include <unordered_map>
 using namespace std;
 
-int unique_words(string word){
-    
+void unique_words(string x, unordered_map<string, int> m){
+    stringstream line(x);
+    string word;
+    while(line >> word){
+        m[word]++;
+    }
+    return;
 }
 
 int main(){
-    string bible = "KJVbible.txt", quran = "quran.txt";
+    string bible = "KJVbible.txt.txt", quran = "quran.txt";
     ifstream input;
+
+    input.open(bible);
 
     if(!input.is_open()){
         return -1;
     }
 
+    unordered_map<string, int> mp;
+
     while(input){
         string line;
-        getline(input, line, ':');
+        getline(input, line);
+        unique_words(line, mp);
     }
-
 
     return 0;
 }
